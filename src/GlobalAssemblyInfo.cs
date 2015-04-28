dd81d6dd6eebc52154411dcef1510e297e045f48
Adrian Brown <adesbrown@gmail.com>
2015-04-28 15:39:45 +0800

    Created a GlobalAssemblyInfo file for versioning in TeamCity.

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("A basic statistical library for .NET")]
+[assembly: AssemblyCompany("Centre for Data Linkage")]
+[assembly: AssemblyProduct("MathStat")]
+[assembly: AssemblyCopyright("Copyright © Adrian Brown 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Warning: Do not update these manuall! These are updated by the automated build processed.
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
--- src/MathStat/Properties/AssemblyInfo.cs
+++ src/MathStat/Properties/AssemblyInfo.cs
@@ -6,32 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MathStat")]
-[assembly: AssemblyDescription("A basic statistical library for .NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Centre for Data Linkage")]
-[assembly: AssemblyProduct("MathStat")]
-[assembly: AssemblyCopyright("Copyright © Adrian Brown 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d92b4d36-ffc0-45ca-8b12-98003d3a9c61")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
